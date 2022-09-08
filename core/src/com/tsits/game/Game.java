e2e333de744c3d4d2eb7df0c87e62ddaa5453632
Owain Gibson <owain.gibson@hotmail.co.uk>
2022-09-08 04:31:21 +0100

    movement minus dodge

--- core/src/com/tsits/game/Game.java
+++ core/src/com/tsits/game/Game.java
@@ -1,32 +1,82 @@
 package com.tsits.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Game extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
 	PlayerCharacter playerCharacter;
+	private OrthographicCamera camera;
+
+	private int xMovement = 0;
+	private int yMovement = 0;
 	
 	@Override
 	public void create () {
+		playerCharacter = new PlayerCharacter();
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 1280, 720);
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(1, 1, 1, 1);
+
+		checkPlayerMovement();
+
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(playerCharacter.sprite, (int)playerCharacter.xPos, (int)playerCharacter.yPos);
 		batch.end();
+
+
 	}
-	
+
+	private void checkPlayerMovement(){
+		if(Gdx.input.isKeyPressed(Input.Keys.A)){
+			playerCharacter.xPos -= playerCharacter.getBaseMoveSpeed() * Gdx.graphics.getDeltaTime();
+			if (playerCharacter.isFacingRight()){
+				playerCharacter.flipSkinX();
+			}
+			xMovement = -1;
+		}
+		else {xMovement = 0;}
+
+		if(Gdx.input.isKeyPressed(Input.Keys.D)){
+			playerCharacter.xPos += playerCharacter.getBaseMoveSpeed() * Gdx.graphics.getDeltaTime();
+			if (!playerCharacter.isFacingRight()){
+				playerCharacter.flipSkinX();
+			}
+			xMovement = 1;
+		}
+
+		if(Gdx.input.isKeyPressed(Input.Keys.W)){
+			playerCharacter.yPos += playerCharacter.getBaseMoveSpeed() * Gdx.graphics.getDeltaTime();
+			yMovement = 1;
+		}
+		else {yMovement = 0;}
+
+		if(Gdx.input.isKeyPressed(Input.Keys.S)){
+			playerCharacter.yPos -= playerCharacter.getBaseMoveSpeed() * Gdx.graphics.getDeltaTime();
+			yMovement = -1;
+		}
+
+		else if(Gdx.input.isKeyJustPressed(Input.Keys.SPACE) && playerCharacter.canDodge()){
+			playerCharacter.isDodging = true;
+		}
+
+	}
+
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
--- core/src/com/tsits/game/PlayerCharacter.java
+++ core/src/com/tsits/game/PlayerCharacter.java
@@ -1,11 +1,78 @@
 package com.tsits.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 
 public class PlayerCharacter {
     private Texture skin;
-    
+    Sprite sprite;
+    float xPos;
+    float yPos;
+
     private Weapon shotgun;
     private Weapon bayonet;
-    private int stamina;
+    private float stamina;
+    private float maxStamina;
+    private float baseMoveSpeed;
+    private boolean facingRight;
+    private float tempMoveSpeed;
+    boolean isDodging;
+
+    public PlayerCharacter(){
+        this.skin = new Texture("manny.png");
+        this.sprite = new Sprite(skin);
+        this.xPos = 600;
+        this.yPos = 310;
+        this.baseMoveSpeed = 120.0f;
+        this.facingRight = false;
+        this.maxStamina = 100f;
+        this.stamina = maxStamina;
+    }
+
+    public float getBaseMoveSpeed() {
+        tempMoveSpeed = baseMoveSpeed;
+        if(Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT) && stamina <= 0f){
+            stamina = 0f;
+        }
+        else{
+            if(Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT) && stamina > 0f){
+                stamina -= 40f * Gdx.graphics.getDeltaTime();
+                tempMoveSpeed = baseMoveSpeed * 2f;
+            }
+            else {
+                if(Gdx.input.isButtonPressed(Input.Buttons.RIGHT)){
+                    tempMoveSpeed = baseMoveSpeed * 0.5f;
+                }
+                stamina += 40f * Gdx.graphics.getDeltaTime();
+                if (stamina > maxStamina){
+                    stamina = maxStamina;
+                }
+            }
+        }
+
+        return tempMoveSpeed;
+    }
+
+    public boolean isFacingRight(){
+        return facingRight;
+    }
+
+    public Texture getSkin() {
+        return skin;
+    }
+
+    public void flipSkinX(){
+        sprite.flip(true, false);
+        facingRight = !facingRight;
+    }
+
+    public boolean canDodge(){
+        if(isDodging) return false;
+        else if(stamina < maxStamina/3) return false;
+
+        return true;
+    }
+
 }
